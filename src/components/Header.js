e290088a815f0357b4e7484c39591624fc5b9c69
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-07-06 12:22:59 -0400

    delete src/logo.svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <h1>Tess Thomas</h1>
-      <h2>Future Full-Stack Developer</h2>
+      <h2>Full-Stack Developer In Progress</h2>
     </header>
   )
 }
