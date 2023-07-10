c70994c88c2ae185bf5ecdb7803fc94825202a3f
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-07-10 18:46:23 -0400

    edit copy

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMe() {
       <div className='about-text'>
         <p>I'm a creative web development student and long-time process nerd with 10+ years experience in art, building, and design. There's nothing I love more than helping people bring ambitious ideas to life.</p>
         <br></br>
-        <p>My extensive background in crafting physical objects for end-users means I'm a big fan of staying curious, and makes doing the same digitally - from UI development to database configuration - feel like a true joy. I graduate in May of 2024 and can't wait to contribute to my new field.</p>
+        <p>My extensive background in crafting physical objects for end-users makes doing the same digitally a real joy. From UI development to database configuration, I'm a big fan of staying curious while keeping an eye on efficiency. I graduate in May of 2024 and can't wait to contribute to my new field.</p>
       </div>
 
         <img className='profile-img' src={headCrop} alt='Tess Thomas' />
