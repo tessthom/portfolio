f2715f5d58d712e099e5317fd6dd2820381382ef
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-07-11 11:36:26 -0400

    cursor mod

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,37 +19,3 @@ function Header() {
 }
 
 export default Header;
-
-// import React, { useState } from 'react';
-// import NavBar from './NavBar';
-// import Toggle from './Toggle';
-// import '../styles/Header.css';
-
-// function Header() {
-//   const [theme, setTheme] = useState('dark');
-
-//   const handleToggle = () => {
-//     if (theme === 'dark') {
-//       setTheme('light');
-//     } else {
-//       setTheme('dark');
-//     }
-//   };
-
-//   return (
-//     <header>
-//       <NavBar theme={theme} />
-//       <div className="header-text">
-//         <div className='inline-container'>
-//           <h1>Hi, I'm Tess Thomas</h1>
-//           <Toggle theme={theme} onChange={handleToggle} />
-//         </div>
-//         <span className='cursor-box'>
-//           <h2>Full-Stack Developer in progress.</h2>
-//         </span>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Header;
